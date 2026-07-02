d56cc61ffa14bcba16ea4fc964f938b9d2ddcf7a
xing lu <105527609+lumenatte@users.noreply.github.com>
2026-07-02 09:25:39 +0800

    main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=56befe67-1859-40bf-b835-1edc2b706ad8",
     # 添加更多链接
 ]
 
